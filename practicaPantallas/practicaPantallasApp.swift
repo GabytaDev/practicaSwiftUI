5465ddf8802f38bbb7c4a7615c84357532ea1f1b
GabytaDev <gabyta.loyola@gmail.com>
2023-03-09 18:40:05 -0300

    Initial Commit

---
+++ practicaPantallas/practicaPantallasApp.swift
@@ -0,0 +1,17 @@
+//
+//  practicaPantallasApp.swift
+//  practicaPantallas
+//
+//  Created by Gabriela Loyola on 09/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct practicaPantallasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
